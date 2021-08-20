a0116470a24ac42c67b19389ea7506c6bb3f9ef4
shakesbeer2049 <max.zubi@gmail.com>
2021-08-20 11:36:15 +0530

    submitted 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,69 +1,25 @@
-import React from 'react'
+import React , {useState} from 'react'
 
-const Header = (props) => {
-  return(
-      <div>
-        <h1>{props.course.name}</h1>
-      </div>
-  )
-}
-const Part = (props)=>{
-  return(
-    <div>
-      <p>
-      {props.part} {props.exercises}
-    </p>
-    </div>
-  )
-}
-
-const Content = (props) => {
+const App = () =>{
+  
+  const randomGen = () => Math.floor(Math.random() * (anecdotes.length))
+  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  
+  const [selected ,setSelected] = useState(0)
   return(
     <div>
-
-    <Part part = {props.contents.parts[0].name} exercises= {props.contents.parts[0].exercises} />
-    <Part part = {props.contents.parts[1].name} exercises= {props.contents.parts[1].exercises} />
-    <Part part = {props.contents.parts[2].name} exercises= {props.contents.parts[2].exercises} />
-    
+      <p>{anecdotes[selected]}</p>
+      <button onClick = {()=> setSelected(randomGen)} >Next anecdote</button>
     </div>
   )
- }
-
-
-const Total = (props) => {
-    return(
-      <div>
-        <p>Total number of exercises is {props.sum.parts[0].exercises+props.sum.parts[1].exercises+props.sum.parts[2].exercises}</p> 
-      </div>
-    )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-
-  parts: [
-    {  
-      name: 'Fundamentals of React',
-      exercises : 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises : 7
-    },
-    {
-      name: 'State of a component',
-      exercises : 14
-    }
-                ]
-              }
-  return(
-    <div>
-      <Header course = {course} />
-      <Content contents={course} />
-      <Total sum= {course} />
-
-    </div>
-    )
 }
 export default App
